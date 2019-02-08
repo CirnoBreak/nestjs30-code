16eb13e10c83ae290128ace1c6fbb0c40397719c
CirnoBreak <1103054869@qq.com>
2019-02-08 18:26:48 +0800

    fix day15

--- day15-rabbitmq/src/app.controller.ts
+++ day15-rabbitmq/src/app.controller.ts
@@ -5,6 +5,11 @@ import { Observable, of } from 'rxjs';
 import { ClientRMQ } from 'nestjs-rmq';
 // import { ClientRMQ } from 'nestjs-rmq';
 
+const clientOpt = {
+  urls: ['amqp://ixtpomaj:t1EdwWivTGtzf9XVu40ZSvNh2gSMy6BQ@mustang.rmq.cloudamqp.com/ixtpomaj'],
+  queue: 'amqptest'
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -12,12 +17,18 @@ export class AppController {
 
   @MessagePattern({ cmd: 'sayHi' })
   sayHi(data: string): Observable<string> {
-    return of("Hi,I'm MicroService.");
+    return of("Hi,I'm " + data);
   }
 
   @MessagePattern({ cmd: 'amqp' })
   useRabbitMQ(data: string): Observable<string> {
     return of(data);
+  }
+
+  @Get('sayhi')
+  callSayHi(): Observable<string> {
+    this.client = new ClientRMQ(clientOpt);
+    return this.client.send<string>({ cmd: 'sayHi' }, 'test.');
   }
 
   @Get('rabbitMQ')
@@ -27,11 +38,7 @@ export class AppController {
     };
 
     const data = 'use RabbitMQ';
-    this.client = new ClientRMQ({
-      urls: ['amqp://ixtpomaj:t1EdwWivTGtzf9XVu40ZSvNh2gSMy6BQ@mustang.rmq.cloudamqp.com/ixtpomaj'],
-      queue: 'amqptest'
-    });
-    console.log('oka')
-    return this.client.send<string, string>(pattern, data);
+    this.client = new ClientRMQ(clientOpt);
+    return this.client.send<string>(pattern, data);
   }
 }
--- day15-rabbitmq/src/main.ts
+++ day15-rabbitmq/src/main.ts
@@ -1,16 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Transport } from '@nestjs/microservices';
-import { ClientRMQ } from 'nestjs-rmq';
+import { ServerRMQ } from 'nestjs-rmq';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice({
-    transport: Transport.RMQ,
-    options: {
-      urls: ['amqp://ixtpomaj:t1EdwWivTGtzf9XVu40ZSvNh2gSMy6BQ@mustang.rmq.cloudamqp.com/ixtpomaj'],
-      queue: 'amqltest'
-    }
+    strategy: new ServerRMQ({
+      urls: ['amqp://ixtpomaj:t1EdwWivTGtzf9XVu40ZSvNh2gSMy6BQ@mustang.rmq.cloudamqp.com/ixtpomaj'], 
+      queue: 'amqptest'}
+    )
   });
 
   await app.startAllMicroservices();
